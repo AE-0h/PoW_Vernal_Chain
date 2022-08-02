5037ddda2479dfe9d484bcaa15d0ba9d07352ec9
Conor Gallagher <dontlisten33@gmail.com>
2022-08-02 16:21:20 -0400

    Redux initial build

--- vernal_Chain/pages/_app.js
+++ vernal_Chain/pages/_app.js
@@ -4,15 +4,20 @@ import { ChakraProvider } from '@chakra-ui/react';
 // 1. Import the extendTheme function
 import { extendTheme } from '@chakra-ui/react';
 import styles from "/styles/globals.css";
+import { wrapper, store } from "../store/store";
+import { Provider } from "react-redux";
+
 
 const theme = extendTheme({ });
 
 // 3. Pass the `theme` prop to the `ChakraProvider`
 function MyApp({ Component, pageProps }) {
   return (
+    <Provider store={store}>
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+    </Provider>
   )
 }
 
