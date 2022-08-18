7f0b327d71d9cc0452c5c873977c77f8cb4b7183
Conor Gallagher <dontlisten33@gmail.com>
2022-08-17 23:59:02 -0400

    faucet build

--- vernal_Chain/components/vernal_cards/faucet_card.js
+++ vernal_Chain/components/vernal_cards/faucet_card.js
@@ -25,10 +25,10 @@ const Faucet_Card = () => {
     <div className={styles.background}>
       <Box sx={innerBoxStyles} backdropFilter="auto" backdropBlur="15px">
         <Flex justifyContent='start' alignItems='start' bg='linear-gradient(to right,  #212121 0%, #383838 100%) ' className={styles.miniHead}>
-          <h1 className={styles.miniHeadText}>FAUCET</h1>
-        <Heading as='h1' size='xl' fontWeight='thin' color='#cff9e0' paddingLeft={2}>
-        </Heading>
       </Flex>
+        <Heading as="h1" size="xl" color="#cff9e0" textShadow="0 1 4px black" fontSize="20px">
+          A simple faucet.
+          </Heading>
       </Box>
       </div>
   );
